a4207452dd9aa73508474e04068f985cba2cc883
koolwilly72 <kitzcloset@gmail.com>
2022-01-11 01:44:45 +0000

    Added my day1 project

---
+++ Day 1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city = input("What city did you grow up in? ")
+fav_pet = input("What is the name of your favorire pet? ")
+print("Your band name is " + city + " " + fav_pet)
